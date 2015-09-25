0152d2f67ee149460014960cfa639bb7f081b5f9
Kevin Häni <kevin.haeni@gmail.com>
2015-09-25 10:09:49 +0200

    namespace fix

--- C64/Utils.cpp
+++ C64/Utils.cpp
@@ -1,7 +1,7 @@
 #include "Utils.h"
 
 namespace Utils{
-	uint16_t Utils::makeWord(uint8_t lsb, uint8_t msb) {
+	uint16_t makeWord(uint8_t lsb, uint8_t msb) {
 		uint16_t word = (msb << 8 | (lsb & 0xFF));
 		return word;
 	}
